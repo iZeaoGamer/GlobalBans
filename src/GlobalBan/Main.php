36e5948659e75b83d75b4485a9627ad2ac0407ad
korado531m7 <support.system@icloud.com>
2016-10-04 23:23:37 +0900

    Added Global Chatting System for Server Owner

--- src/GlobalBan/Main.php
+++ src/GlobalBan/Main.php
@@ -17,7 +17,7 @@ class Main extends PluginBase implements Listener{
 	public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder(), 0777, true);
-        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array('Language' => 'en','LinkDefaultBan' => 'true','BanMessage' => 'You were banned by admin','RTAlert' => 'false','license' => null));
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array('Language' => 'en','LinkDefaultBan' => 'true','BanMessage' => 'You were banned by admin','RTAlert' => 'false','license' => null,'ChattingReciever' => 'true','muteid' => null));
         
         $this->getLogger()->notice($this->getMessage("Connecting to the GlobalBan Database...","グローバルBANデータベースに接続しています..."));
         
@@ -32,6 +32,8 @@ class Main extends PluginBase implements Listener{
         }else{
             if($database->{'status'} == "true"){
                 $this->getLogger()->notice($this->getMessage("Connected to the GlobalBan Database","グローバルBANデータベースに接続しました"));
+                Utils::postURL("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=statuspost&license=".$this->getLicense(), ["status" => "true","name" => $this->getServer()->getMotd(),"license" => $this->getLicense()]);
+                
                 $this->getLogger()->info($this->getMessage("Enabled GlobalBan Plugin and Turned on the GBan System","グローバルBANプラグインが有効になり、GBANが有効になりました"));
                 $this->getLogger()->notice($this->getMessage("Using License: ","使用しているライセンス: ").$this->getLicense());
                 $this->getLogger()->notice($this->getMessage("Official GBan URL:","公式GbanサイトのURL: ")."http://korado531m7.php.xdomain.jp/gban");
@@ -42,15 +44,49 @@ class Main extends PluginBase implements Listener{
                 $this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin("GlobalBans"));
             }
         }
+        if($this->getServer()->getPluginManager()->getPlugin("GlobalChat")){
+            $this->getLogger()->warning($this->getMessage('Please Remove GlobalChat Plugin or This','このプラグインかグローバルチャットを抜いてください'));
+            $this->getServer()->forceShutdown();
+        }
         $this->logo = "§a[GlobalBan] §f";
         if($this->config->get("RTAlert") == "true"){
             $this->set = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=rt&license=".$this->getLicense()));
             $this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask([$this, "realTimeShower"]), 35);
         }
+        if($this->config->get("ChattingReciever") == "true"){
+            $this->gchat = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=chatget&license=".$this->getLicense()));
+            $this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask([$this, "chattingReciever"]), 35);
+        }
+    }
+    
+    public function onDisable(){
+        Utils::postURL("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=statuspost&license=".$this->getLicense(), ["status" => "false", "license" => $this->getLicense()]);
     }
     
     private function getLicense(){
         return $this->config->get("license");
+    }
+    
+    function chattingReciever(){
+        if(empty(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=chatget&license=".$this->getLicense()))){
+            return;
+        }else{
+            $chatobj = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=chatget&license=".$this->getLicense()));
+            if($this->gchat != $chatobj){
+                foreach($this->config->get("muteid") as $chatid){
+                    if($chatid["id"] == $chatobj->{'id'}){
+                        $this->gchat = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=chatget&license=".$this->getLicense()));
+                        return;
+                    }
+                }
+                $this->getLogger()->notice("§6+------------- ".$this->getMessage('Chatting System','チャットシステム')." -------------+");
+                $this->getLogger()->notice("§b+".$this->getMessage('Server Name','サーバー名')." §7: §e".$chatobj->{'name'});
+                $this->getLogger()->notice("§b+".$this->getMessage('Server ID','サーバーID')."   §7: §e".$chatobj->{'id'});
+                $this->getLogger()->notice("§b+".$this->getMessage('Message','メッセージ')."     §7: §e".$chatobj->{'message'});
+                $this->getLogger()->notice("§6+-------------------------------------------+");
+                $this->gchat = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=chatget&license=".$this->getLicense()));
+            }
+        }
     }
     
     function realTimeShower(){
@@ -58,7 +94,7 @@ class Main extends PluginBase implements Listener{
             return;
         }else{
             $db = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=rt&license=".$this->getLicense()));
-            if(empty($this->set)){
+            if($this->set != $db){
                 if($this->config->get("Language") == "jp"){
                     $this->getLogger()->notice("§6+------------- リアルタイム通知 -------------+");
                     $this->getLogger()->notice("§bBANしたサーバー名 : ".$db->{'ServerName'});
@@ -75,12 +111,6 @@ class Main extends PluginBase implements Listener{
                     $this->getLogger()->notice("§6+-----------------------------------------+");
                 }
                 $this->set = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=rt&license=".$this->getLicense()));
-            }else{
-                if($db == $this->set){
-                    return;
-                }else{
-                    unset($this->set);
-                }
             }
         }
     }
@@ -102,37 +132,8 @@ class Main extends PluginBase implements Listener{
                 switch(strtolower($args[0])){
                       
                     default:
-                        $sender->sendMessage($this->getMessage("§6----------- GlobalBan Command -----------\n§e/gban <Name> <Player Name> <Description>\n§e/gban <Cid> <Client ID> <Description>\n§e/gban <Ip> <IP Address> <Description>\n§e/gban <All> <Player Name> <Description>\n§e/gban <List> <Name/Ip/Cid>\n§6--------------------------------------------","§6----------- GlobalBan コマンド -----------\n§e/gban <Name> <プレイヤー名> <BANの説明>\n§e/gban <Cid> <Client ID> <BANの説明>\n§e/gban <Ip> <IPアドレス> <BANの説明>\n§e/gban <All> <プレイヤー名> <BANの説明>\n§e/gban <List> <Name/Ip/Cid>\n§6--------------------------------------------"));
+                        $sender->sendMessage($this->getMessage("§6----------- GlobalBan Command -----------\n§e/gban <Name> <Player Name> <Description>\n§e/gban <Cid> <Client ID> <Description>\n§e/gban <Ip> <IP Address> <Description>\n§e/gban <All> <Player Name> <Description>\n§e/gban <List> <Name/Ip/Cid>\n§e/gban <Status>\n§e/chat <Server ID> <Message>\n§e/chatmute <Server ID>\n§6--------------------------------------------","§6----------- GlobalBan コマンド -----------\n§e/gban <Name> <プレイヤー名> <BANの説明>\n§e/gban <Cid> <Client ID> <BANの説明>\n§e/gban <Ip> <IPアドレス> <BANの説明>\n§e/gban <All> <プレイヤー名> <BANの説明>\n§e/gban <List> <Name/Ip/Cid>\n§e/gban <Status>\n§e/chat <サーバーID> <メッセージ>\n§e/chatmute <サーバーID>\n§6--------------------------------------------"));
                     break;
-                        
-                    /*case "check":
-                        if(empty($args[1])){
-                            $sender->sendMessage($this->getMessage("§e/gban <Check> <Connection/Name/Cid/Ip> <Value>","§e/gban <Check> <Name/Cid/Ip> <内容>"));
-                        }else{
-                            switch(strtolower($args[1])){
-                                default:
-                                    
-                                break;
-                                    
-                                case "connection":
-                                    
-                                break;
-                                    
-                                case "name":
-                                    $db = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=check&name=".strtolower($player->getName())));
-                                break;
-                                    
-                                case "cid":
-                                    $db = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=check&cid=".$player->getClientId()));
-                                break;
-                                    
-                                case "ip":
-                                    $db = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=check&ip=".$player->getAddress()));
-                                    $sender->sendMessage("");
-                                break;
-                            }
-                        }
-                    break;*/
                         
                     case "list":
                         if(empty($args[1])){
@@ -174,64 +175,26 @@ class Main extends PluginBase implements Listener{
                                 $datum = $this->getServer()->getPlayer($name)->getRawUniqueId();
                                 $this->getServer()->getPlayer($name)->kick($this->config->get("BanMessage"),false);
                             }
-                            $sender->sendMessage($this->logo.$this->getMessage("§9> §dClient Ban Data has been posted","§9> §dRawUniqueID Banのデータが送信されました"));
-                            $this->getLogger()->notice($this->getMessage("§9> §bClient Ban Data Has Been Posted. Posted Client ID:","§9> §bRawUniqueID BANデータが送信されました。 送信されたID:").$datum);
+                            $sender->sendMessage($this->logo.$this->getMessage("§9> §dRawUniqueID Ban Data has been posted","§9> §dRawUniqueID Banのデータが送信されました"));
+                            $this->getLogger()->notice($this->getMessage("§9> §bRawUniqueID Ban Data Has Been Posted. Posted Client ID:","§9> §bRawUniqueID BANデータが送信されました。 送信されたID:").$datum);
                         }
                     break;
                     */
                     
-                    /*
                     case "chat":
-                        if(empty($args[2])){
-                            $sender->sendMessage($this->getMessage("§9> §e/gban <Chat> <Server ID> <Message>","§9> §e/gban <Chat> <サーバーID> <メッセージ>"));
-                        }else{
-                            $chat = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=servercheck&serverid=".$args[1]."&license=".$this->getLicense()));
-                            $servid = $args[1];
-                            array_shift($args);
-                            array_shift($args);
-                            if($sender instanceof Player){
-                                $sender->sendMessage($this->getMessage("§9> §dThis Command Can Use On Console Only","§9> §dこのコマンドはコンソール専用になっています"));
-                                return;
-                            }
-                            if($chat->{'settings'} == false){
-                                $sender->sendMessage($this->getMessage("§9> §dChat Partner Selecting to Rejecting Mode, So Message Couldn't Send","§9> §dチャット相手は拒否モードにしているためチャットを送信できませんでした"));
-                                return;
-                            }
-                     
-                            if($chat->{'active'} == true){
-                                if($chat->{'status'} == true){
-                                    Utils::postURL("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=chat&serverid=".$servid."&license=".$this->getLicense(), ["type" => "cid","value" => $name,"servername" => $this->getServer()->getMotd(),"description" => implode(" ",$args),"license" => $this->getLicense()]);
-                                    $sender->sendMessage($this->getMessage("§9> §aChat Sent Successful","§9> §aチャットが送信されました"));
-                                }else{
-                                    $sender->sendMessage($this->getMessage("§9> §aChat Couldn't Send, Because Partner Is Not Online Now","§9> §aチャット相手はオンラインではないため、送信できませんでした"));
-                                }
-                            }else{
-                                $sender->sendMessage($this->getMessage("§9> §dThat Server ID Is Not Registered Yet","§9> §dそのサーバーIDは未登録です"));
-                            }
-                        }
+                        $sender->sendMessage($this->getMessage("§9> §e/chat <Server ID> <Message>\n§9> §e/chatmute <Server ID>","§9> §e/chatmute <サーバーID>"));
                     break;
-                    */
                     
-                    /*
-                    case "server":
-                        if(empty($args[1])){
-                            $sender->sendMessage($this->getMessage("§9> §e/gban <Server> <OnlineList>","§9> §e/gban <Server> <OnlineList>"));
-                        }else{
-                            if($sender instanceof Player){
-                                $sender->sendMessage($this->getMessage("§9> §dThis Command Can Use On Console Only","§9> §dこのコマンドはコンソール専用になっています"));
-                                return;
-                            }
-                            $this->getLogger()->notice($this->getMessage("Loading Database...","データベースを読み込み中..."));
-                            $listdata = file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=serverstatus&license=".$this->getLicense());
-                            $this->getLogger()->info("+--------------------- Servers Status--------------------+\n".$this->getMessage($listdata,str_replace(array("STATUS:","SERVER ID:"),array("状況:","サーバーID:"),$listdata)));
-                     
-                             // koradoサーバー | SERVER ID: 8983284613115381 | STATUS: ONLINE
-                     
-                            
-                            $this->getLogger()->info("+--------------------------------------------------------+");
+                    case "status":
+                        if($sender instanceof Player){
+                            $sender->sendMessage($this->getMessage("§9> §dThis Command Can Use On Console Only","§9> §dこのコマンドはコンソール専用になっています"));
+                            return;
                         }
+                        $this->getLogger()->notice($this->getMessage("Getting Status from Database...","データベースから状況を取得中..."));
+                        $listdata = file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=serverstatus&license=".$this->getLicense());
+                        $this->getLogger()->info("+--------------------- Online Servers --------------------+\n".$this->getMessage($listdata,str_replace(array("STATUS:","SERVER ID:"),array("状況:","サーバーID:"),$listdata)));
+                        $this->getLogger()->info("+---------------------------------------------------------+");
                     break;
-                     */
                         
                     case "cid":
                         if(empty($args[1])){
@@ -359,6 +322,52 @@ class Main extends PluginBase implements Listener{
                             $this->getLogger()->notice($this->getMessage("§9> §bName Ban Data Has Been Posted. Posted Minecraft Name:","§9> §b名前 BANデータが送信されました。 送信されたマインクラフト名:").$datum);
                         }
                         break;
+                }
+                break;
+                
+            case "chatmute":
+                if(empty($args[0])){
+                    $sender->sendMessage($this->getMessage("§9> §e/chatmute <Server ID>","§9> §e/chatmute <サーバーID>"));
+                }else{
+                    if($sender instanceof Player){
+                        $sender->sendMessage($this->getMessage("§9> §dThis Command Can Use On Console","§9> §dこのコマンドはコンソール上からのみ可能です>"));
+                    }else{
+                        $tmp = array();
+                        $tmp["id"] = $args[0];
+                        $this->muteids[] = $tmp;
+                        $this->config->set("muteid",$this->muteids);
+                        $this->config->save();
+                        $sender->sendMessage($this->getMessage("§9> §aServer ID §e".$args[0]."§a Has Been Muted","§9> §aサーバー ID §e".$args[0]."§a をミュートしました"));
+                    }
+                }
+            break;
+                
+            case "chat":
+                if(empty($args[1])){
+                    $sender->sendMessage($this->getMessage("§9> §e/chat <Server ID> <Message>","§9> §e/chat <サーバーID> <メッセージ>"));
+                }else{
+                    if($sender instanceof Player){
+                        $sender->sendMessage($this->getMessage("§9> §dThis Command Can Use On Console","§9> §dこのコマンドはコンソール上からのみ可能です>"));
+                    }else{
+                        $servid = $args[0];
+                        $chat = json_decode(file_get_contents("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=statusget&serverid=".$servid."&license=".$this->getLicense()));
+                        array_shift($args);
+                        if($chat->{'settings'} == "false"){
+                            $sender->sendMessage($this->getMessage("§9> §dChat Partner Selecting to Rejecting Mode, So Message Couldn't Send","§9> §dチャット相手は拒否モードにしているためチャットを送信できませんでした"));
+                            return;
+                        }
+                        
+                        if($chat->{'active'} == "true"){
+                            if($chat->{'status'} == "true"){
+                                Utils::postURL("http://korado531m7.php.xdomain.jp/gban/gbansystem.php?sys=chatpost&serverid=".$servid."&license=".$this->getLicense(), ["message" => implode(" ",$args),"name" => $this->getServer()->getMotd(),"license" => $this->getLicense()]);
+                                $sender->sendMessage($this->getMessage("§9> §aChat Sent Successful","§9> §aチャットが送信されました"));
+                            }else{
+                                $sender->sendMessage($this->getMessage("§9> §aChat Couldn't Send, Because Partner Is Not Online Now","§9> §aチャット相手はオンラインではないため、送信できませんでした"));
+                            }
+                        }else{
+                            $sender->sendMessage($this->getMessage("§9> §dThat Server ID Is Not Registered Yet","§9> §dそのサーバーIDは未登録です"));
+                        }
+                    }
                 }
                 break;
         }
